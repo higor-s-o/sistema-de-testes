83652e4150d2b6e5a79eea14b63579d0a5d25c57
higor-s-o <higorsouz@gmail.com>
2023-08-29 05:40:32 -0300

    Realizando teste de duble e insercao com massa de dados

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -1,8 +1,25 @@
 describe('Página de Login', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/#/home')
+
+    cy.intercept('POST', 'http://localhost:3000/user/login', {
+      statusCode: 400
+    }).as('stubPost')
+
+  })
+
     it('Verificar mensagens de campo obrigatorios', () => {
-      cy.visit('http://localhost:4200/#/home')
       cy.contains('User name is required!').should('be.visible');
       cy.contains('Password is required!').should('be.visible');
       
     })
+
+    it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+      cy.login('carowl', '12345678');
+      cy.wait('@stubPost')
+      
+    })
+    
+
   })
